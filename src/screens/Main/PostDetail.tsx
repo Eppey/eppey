fe567b6bbe463a566ecdf9752b5cbf848f150db3
Sehoan Choi <sehoanchoi0124@gmail.com>
2021-12-29 21:52:33 -0500

    Positioned text data into the right order and modified their styles

--- src/screens/Main/PostDetail.tsx
+++ src/screens/Main/PostDetail.tsx
@@ -10,6 +10,7 @@ import * as queries from '../../graphql/queries';
 
 import { API } from 'aws-amplify';
 import { Post, GetPostQuery, GetPostQueryVariables } from '../../API';
+import { fonts } from '../../styles/fonts';
 
 const PostDetail = ({ route, navigation }: any) => {
   const [post, setPost] = useState({} as Post);
@@ -25,17 +26,53 @@ const PostDetail = ({ route, navigation }: any) => {
     })();
   }, []);
 
+  const timeElapsed = (createdAt: string) => {
+    const created = new Date(createdAt);
+    const now = new Date();
+    let unit = 's';
+    let elapsed = (now.getTime() - created.getTime()) / 1000;
+    if (elapsed > 365 * 24 * 60 * 60) {
+      elapsed /= 365 * 24 * 60 * 60;
+      unit = 'y';
+    } else if (elapsed > 30 * 24 * 60 * 60) {
+      elapsed /= 30 * 24 * 60 * 60;
+      unit = 'm';
+    } else if (elapsed > 7 * 24 * 60 * 60) {
+      elapsed /= 7 * 24 * 60 * 60;
+      unit = 'w';
+    } else if (elapsed > 24 * 60 * 60) {
+      elapsed /= 24 * 60 * 60;
+      unit = 'd';
+    } else if (elapsed > 60 * 60) {
+      elapsed /= 60 * 60;
+      unit = 'h';
+    } else if (elapsed > 60) {
+      elapsed /= 60;
+      unit = 'm';
+    }
+    return Math.round(elapsed) + unit;
+  };
+
   return (
-    <View>
+    <View style={styles.container}>
       <StatusBar barStyle="light-content" />
       <View>
         {Object.keys(post).length === 0 ? (
           <ActivityIndicator size="large" color="#272F40" />
         ) : (
           <View>
-            <Text>{post.title}</Text>
-            <Text>{post.topic}</Text>
-            <Text>{post.content}</Text>
+            <Text style={styles.title}>{post.title}</Text>
+            <Text style={{ ...styles.subdetail, marginBottom: 2 }}>
+              {post.topic}
+            </Text>
+            <Text style={styles.subdetail}>
+              {post.userNickname} | {timeElapsed(post.createdAt)}
+            </Text>
+            <View style={styles.contentContainer}>
+              <Text style={styles.content}>
+                {post.content.replace(/\r?\n|\r/g, ' ')}
+              </Text>
+            </View>
           </View>
         )}
       </View>
@@ -44,25 +81,27 @@ const PostDetail = ({ route, navigation }: any) => {
 };
 
 const styles = StyleSheet.create({
-  titleField: {
-    ...fonts.body1Black,
-    ...{
-      fontWeight: 'normal',
-      height: 50,
-      borderBottomWidth: 1,
-      borderBottomColor: '#272F4026',
-    },
+  container: {
+    paddingHorizontal: 20,
+    paddingVertical: 30,
   },
-  bodyField: {
-    ...fonts.body1Black,
-    ...{
-      fontWeight: 'normal',
-      paddingTop: 15,
-    },
+  title: {
+    ...fonts.header2,
+    marginBottom: 15,
   },
-  pickerText: {
-    ...fonts.body1Black,
-    ...{ textAlign: 'left', height: 120 },
+  contentContainer: {
+    borderStyle: 'solid',
+    borderTopColor: 'rgba(39, 47, 64, 0.15)',
+    borderTopWidth: 1,
+    marginTop: 15,
+    paddingTop: 15,
+  },
+  content: {
+    ...fonts.body1Light,
+    color: '#272F40',
+  },
+  subdetail: {
+    ...fonts.body1Light,
   },
 });
 
