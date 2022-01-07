ecd937791afdf6939854f532cb6e0e8c34b87e78
Sehoan Choi <sehoanchoi0124@gmail.com>
2022-01-06 19:12:45 -0500

    Added nickname editing feature into an edit button

--- src/screens/MyPage/EditProfile.tsx
+++ src/screens/MyPage/EditProfile.tsx
@@ -1,21 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
-import { majors } from '../../data/majors';
 import { components } from '../../styles/components';
 import { fonts } from '../../styles/fonts';
-import { Auth } from 'aws-amplify';
+import { API, Auth } from 'aws-amplify';
 
-const EditProfile = () => {
-  const [user, setUser] = useState({} as any);
+import * as mutations from '../../graphql/mutations';
+
+const EditProfile = ({ navigation }: any) => {
+  const [userAttr, setUserAttr] = useState({} as any);
 
   useEffect(() => {
     (async () => {
       const { attributes } = await Auth.currentUserInfo();
-      setUser(attributes);
-      console.log(attributes);
+      setUserAttr(attributes);
     })();
   }, []);
+
+  const updateUserAttr = async () => {
+    const params = {
+      id: userAttr.sub,
+      nickname: userAttr.nickname,
+    };
+    await API.graphql({
+      query: mutations.updateUser,
+      variables: { input: params },
+    });
+    const user = await Auth.currentAuthenticatedUser();
+    await Auth.updateUserAttributes(user, { nickname: userAttr.nickname });
+    navigation.goBack();
+  };
 
   return (
     <>
@@ -27,26 +40,19 @@ const EditProfile = () => {
           style={components.inputField}
           autoCapitalize="none"
           autoCorrect={false}
-          onChangeText={(value) => {}}
-          value={user.nickname}
+          onChangeText={(value) =>
+            setUserAttr({ ...userAttr, nickname: value })
+          }
+          value={userAttr.nickname}
         />
-        <Text style={[styles.body1Light, { fontWeight: 'bold' }]}>Major</Text>
-        <Picker
-          selectedValue={user['custom:major']}
-          onValueChange={(itemValue, idx) => console.log('hi')}
-        >
-          {majors.map((item) => (
-            <Picker.Item label={item} value={item} key={item} />
-          ))}
-        </Picker>
       </View>
       <View style={{ alignItems: 'center' }}>
         <Text style={[styles.body1Light, { textAlign: 'center' }]}>
-          By signing up, you’re agreeing to{'\n'}EPPEY’s{' '}
+          This updated nickname will be displayed to other users {'\n'}
           <Text style={{ fontWeight: 'bold' }}>Terms of Service</Text> and
           <Text style={{ fontWeight: 'bold' }}> Privacy Policy</Text>
         </Text>
-        <Pressable style={styles.button} onPress={() => console.log('sign up')}>
+        <Pressable style={styles.button} onPress={updateUserAttr}>
           <Text style={fonts.fButton}>Save</Text>
         </Pressable>
       </View>
@@ -57,9 +63,8 @@ export default EditProfile;
 
 const styles = StyleSheet.create({
   container: {
-    marginHorizontal: '5%',
+    margin: '5%',
   },
-  signup: { ...fonts.header1, ...{ marginTop: 20, marginBottom: 25 } },
   button: { ...components.button, ...{ marginTop: 30 } },
   body1Light: { ...fonts.body1Light, ...{ marginBottom: 8 } },
 });
